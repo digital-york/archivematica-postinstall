9a8346ea06fd86276c259836bf6e5e372524756d
Fergus McGlynn <theferg@gmail.com>
2017-04-05 22:00:46 +0000

    added some progress output

--- postinstall.sh
+++ postinstall.sh
@@ -2,12 +2,15 @@
 
 # update the mysql database (which will affect archivematica)
 mysql -u archivematica -pdemo MCP < ./mysql_changes.sql
+echo "Archivematica mysql database updated"
 # overwrite the Storage Service sqlite database (which will affect storage service)
 sudo cp ./storage.db /var/archivematica/storage-service/storage.db
 sudo chown archivematica:archivematica /var/archivematica/storage-service/storage.db
+echo "Storage Service database replaced"
 # put the processing configuration files in place
 sudo cp ./defaultProcessingMCP.xml ./datasetsProcessingMCP.xml /var/archivematica/sharedDirectory/sharedMicroServiceTasksConfigs/processingMCPConfigs/
 sudo chown archivematica:archivematica /var/archivematica/sharedDirectory/sharedMicroServiceTasksConfigs/processingMCPConfigs/*
+echo "Processing configuration files deployed"
 # install UoY version of automation tools
 cd /usr/lib/archivematica
 sudo git clone https://github.com/digital-york/automation-tools.git
@@ -24,4 +27,4 @@ sudo chown archivematica:archivematica /var/archivematica/automation-tools
 sudo mkdir /etc/archivematica/automation-tools
 sudo cp /etc/transfers.conf /etc/archivematica/automation-tools/transfers.conf
 sudo chown -R archivematica:archivematica /etc/archivematica/automation-tools
-
+echo "UoY automation tools installed"

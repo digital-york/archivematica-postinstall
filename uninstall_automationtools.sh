4df37d8d7e43d41d1aa3b138d90835efec371ba2
Fergus McGlynn <fergus.a.mcglynn@york.ac.uk>
2017-04-06 09:00:19 +0000

    added script to uninstall the UoY automation tools code

---
+++ uninstall_automationtools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm -rf /usr/lib/archivematica/automation-tools
+sudo rm -rf /usr/share/python/automation-tools
+sudo rm -rf /var/log/archivematica/automation-tools
+sudo rm -rf /var/archivematica/automation-tools
+sudo rm -rf /etc/archivematica/automation-tools
